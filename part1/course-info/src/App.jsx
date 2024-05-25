f7bdb0bcb8e96ebe0b601c2f9cb9d29acc61c65b
americobcn <americo.cot@gmail.com>
2024-05-25 17:02:39 +0200

    Completed exercise 1.4

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -25,31 +25,35 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  console.log("Total: ", props)
   return (
-    <p>Number of exercises {props.total}</p>
+    <p> Number of exercises {
+      props.total[0].exercises + props.total[1].exercises + props.total[2].exercises
+    }
+    </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={parts} />
     </div>
   )
 }
