099aace3c88d8a900e3a90796ae8c4b048a56220
americobcn <americo.cot@gmail.com>
2024-06-09 00:18:36 +0200

    Excercis 2.11 part2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Filter = (props) => {
   return (
@@ -37,17 +39,21 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('What\'s your name?')
   const [newPhone, setNewPhone] = useState('What\'s your phone number?')
   const [personsFiltered, setPersonsFiltered] = useState(persons)
   const [filter, setFilter] = useState(false)
+
+  useEffect(() => {
+    console.log("Effect");
+    axios
+    .get("http://localhost:3001/persons")
+    .then(resp => {
+      console.log("Response fullfilled: ", resp)
+      setPersons(resp.data)
+    })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
